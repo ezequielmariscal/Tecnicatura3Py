451fa1d0854eb0dafaa5c7e448a5e1071bbe2da1
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-10 20:23:08 -0300

    Add_delete_register_varios

---
+++ Leccion04/BD/delete_v_registros.py
@@ -0,0 +1,26 @@
+# COMO ELIMINAR VARIOS REGISTROS
+
+import psycopg2
+# Esto es para poder conectarnos a Postgres
+
+conexion = psycopg2.connect(  # Creamos objeto Tipo conexion para hacer la conexion
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_person IN %s'
+            entrada = input('Digite los numeros de registros a eliminar (separados por coma): ')
+            valores = (tuple(entrada.split(',')),)  # tupla de tuplas
+            cursor.executemany(sentencia, valores)   # De esta manera ejecutamos la sentencia
+            registros_eliminados = cursor.rowcount
+            print(f'Los registros eliminados son: {registros_eliminados}')
+
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+   conexion.close()
