858a0eb0a4fb4731e34aefa35e34cc7992cc7d16
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-20 10:27:08 -0300

    add_transacciones_part2

--- Leccion04/BD/transacciones.py
+++ Leccion04/BD/transacciones.py
@@ -9,13 +9,15 @@ conexion = bd.connect(  # Creamos objeto Tipo conexion para hacer la conexion
     database='test_bd'
 )
 try:
-    conexion.autocommit = False  # lo hacemos para que no se guarden los cambios automaticamente
+    #  conexion.autocommit = False  # lo hacemos para que no se guarden los cambios automaticamente # Poner autocommit true es mala practica # esto direcatamente no deberia estar
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email)VALUES(%s, %s, %s)'
     valores = ('Maria', 'Esparza', 'mesparza@mail.com')
     cursor.execute(sentencia, valores)
+    conexion.commit()  # Hacemos el commit manualmente
     print('Termina la transaccion')
 except Exception as e:
-    print(f'Ocurrio un error: {e}')
+    conexion.rollback()  # Lo hacemos para que el commit vuelva para atras y no quede nada en la bd
+    print(f'Ocurrio un error, se hizo un rollback: {e}')
 finally:
    conexion.close()
