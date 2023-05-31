4467edee717f99fdad9096a021a1ae6393937a7f
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-31 01:27:55 -0300

    Add_classPersona_part2

--- Leccion07/01-Loggin/capa_datos_persona/Persona.py
+++ Leccion07/01-Loggin/capa_datos_persona/Persona.py
@@ -1,5 +1,7 @@
+from logger_base import log
+
 class Persona:
-    def __init__(self, id_persona, nombre, apellido, email):
+    def __init__(self, id_persona=None, nombre=None, apellido=None, email=None):
         self._id_persona = id_persona
         self._nombre = nombre
         self._apellido = apellido
@@ -13,29 +15,40 @@ class Persona:
             Email: {self._email}
         '''
 
-# Metodo Getters and Setters
-@property
-def id_persona(self):
-    return self._id_persona
+    # Metodo Getters and Setters
+    @property
+    def id_persona(self):
+        return self._id_persona
 
-@id_persona.setter
-def id_persona(self, id_persona):
-    self._id_persona = id_persona
-@property
-def nombre(self):
-    return self._nombre
-@nombre.setter
-def nombre(self, nombre):
-    self._nombre = nombre
-@property
-def apellido(self):
-    return self._apellido
-@apellido.setter
-def apellido(self, apellido):
-    self._apellido = apellido
-@property
-def email(self):
-    return self._email
-@email.setter
-def email(self, email):
-    self._email = email
+    @id_persona.setter
+    def id_persona(self, id_persona):
+        self._id_persona = id_persona
+    @property
+    def nombre(self):
+        return self._nombre
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+    @property
+    def apellido(self):
+        return self._apellido
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+    @property
+    def email(self):
+        return self._email
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+# Comprobacion si estamos ejecutando en esta clase
+if __name__ == '__main__':
+    persona1 = Persona(1, 'Juan', 'Perez', 'jperez@mail.com')
+    log.debug(persona1)
+# Juguemos un poco con el codigo
+# que sucede si ingresamos cosas erroneas?
+    persona2 = Persona(nombre='Jose', apellido='Lepez', email='ljose@mail.com')
+    log.debug(persona2) # Como soluciono esto?? puedo agregarle id_persona=None
+    persona1 = Persona(id_persona = 1) # con esto elimionamos persona 1
+    log.debug(persona1)
