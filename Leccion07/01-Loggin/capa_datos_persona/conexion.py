4dd154e06216d6dcd72768899a27094ad88decfd
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-06-20 22:25:53 -0300

    Add_class10_par1/2

--- Leccion07/01-Loggin/capa_datos_persona/conexion.py
+++ Leccion07/01-Loggin/capa_datos_persona/conexion.py
@@ -1,4 +1,4 @@
-import psycopg2 as bd
+from psycopg2 import pool
 from logger_base import log
 import sys
 
@@ -9,42 +9,28 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host=cls._HOST,
-                                           user=cls._USERNAME,
-                                           password=cls._PASSWORD,
-                                           port=cls._DB_PORT,
-                                           database=cls._DATABASE)
-                log.debug(f'Conexión Exitosa: {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-        else:
-            return cls._conexion
+        pass
 
     # En caso de error se termina el programa. si es diferente de NONE el objeto esta abierto
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
+        pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
             try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
-                return cls._cursor
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-        else:
-            return cls._cursor
+                cls._pool = pool.SimpleConnectionPool()
+
 
 
 # Hacemos ahora uan prueba de errores
 if __name__ == '__main__':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    pass
+
