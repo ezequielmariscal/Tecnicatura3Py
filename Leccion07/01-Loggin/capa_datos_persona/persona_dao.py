66d2496bf9e70ac8f5ce7b2d91a1664ff0adaec5
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-07-01 21:51:27 -0300

    Add_clas11_part8_finished

--- Leccion07/01-Loggin/capa_datos_persona/persona_dao.py
+++ Leccion07/01-Loggin/capa_datos_persona/persona_dao.py
@@ -57,7 +57,7 @@ class PersonaDAO:
 
 if __name__ == '__main__':
     # Eliminar un registro
-    persona1 = Persona(id_persona= 8)
+    persona1 = Persona(id_persona=18)
     personas_eliminadas = PersonaDAO.eliminar(persona1)
     log.debug(f'Persona eliminada: {personas_eliminadas}')
 
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     log.debug(f'Personas actualizadad: {personas_actualizadas}')
 
     # Insertar un registro
-    persona1 = Persona(nombre='Marcos', apellido='Tejada', email='tejadam@mail.com')
+    persona1 = Persona(nombre='Mateo', apellido='Torres', email='tmateo@mail.com')
     personas_insertadas = PersonaDAO.insertar(persona1)
     log.debug(f'Personas insertada: {personas_insertadas}')
 
