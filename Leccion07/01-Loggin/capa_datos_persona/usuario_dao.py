59e1e5534dc56eb75b76e1335f093387e959266f
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-08-18 00:35:14 -0300

    Add_class01_part4_4toSemestre

---
+++ Leccion07/01-Loggin/capa_datos_persona/usuario_dao.py
@@ -0,0 +1,57 @@
+from capa_datos_persona.cursor_del_pol import CursorDelPool
+from logger_base import log
+
+
+class UsurioDAO:
+    """
+    DAO -> Data Access Object para la tabla de usuario
+    CRUD -> Create - Read - Update - Delete
+                    Create -> Insertar
+                    Read   -> Seleccionar
+                    Update -> Actualizar
+                    Delete -> Eliminar
+    """
+
+    _SELECCIONAR = 'SELECT * FROM usuario ORDER BY id_usuario'
+    _INSERTAR = 'INSERT INTO usuario(username, password) VALUES (%s, %s)'
+    _ACTUALIZAR = 'UPDATE usuario SET username=%s, passwprd=%s WHERE id_usuario=%s'
+    _ELIMINAR = 'DELETE FROM usuario WHERE id_usuario=%s'
+
+    # Definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with CursorDelPool() as cursor:
+            log.debug('Seleccionando usuarios')
+            cursor.execute(cls._SELECCIONAR)
+            registros = cursor.fetchall()
+            usuarios = []  # creamos una lista
+            for registro in registros:
+                usuario = Usuario(registro[0], registro[1], registro[2])
+                usuario.append(usuario)
+            return usuarios
+
+    @classmethod
+    def insertar(cls, usuario):
+        with CursorDelPool() as cursor:
+            log.debug(f'Usuario a insertar: {usuario}')
+            valores = (usuario.username, usuario.password)
+            cursor.execute(cls._INSERTAR, valores)
+            log.debug(f'Persona Insertada: {persona}')
+            return cursor.rowcount
+
+    @classmethod
+    def actualizar(cls, usuario):
+        with CursorDelPool() as cursor:
+            log.debug(f'Usuario a actualizar: {usuario}')
+            valores = (usuario.username, usuario.password, usuario.id_usuario)
+            cursor.execute(cls._ACTUALIZAR, valores)
+            return cursor.rowcount
+
+    @classmethod
+    def eliminar(cls, usuario):
+        with CursorDelPool() as cursor:
+            log.debug(f'Usuario a eliminar: {usuario}')
+            valores = (usuario.id_usuario,)
+            cursor.execute(cls._ELIMINAR, valores)
+
+            return cursor.rowcount
