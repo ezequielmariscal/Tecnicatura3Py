2cb4d9e9c8f497d46eda33c107ee2db3b3be2507
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-08-23 23:28:40 -0300

    Add_class1_part8_4tosem

--- Leccion07/01-Loggin/capa_datos_persona/menu_app_usuario.py
+++ Leccion07/01-Loggin/capa_datos_persona/menu_app_usuario.py
@@ -1,5 +1,6 @@
 from usuario_dao import UsuarioDAO
 from logger_base import log
+from Usuario import Usuario
 opcion = None
 while opcion != 5:
     print('Opciones: ')
@@ -13,7 +14,12 @@ while opcion != 5:
         usuarios = UsuarioDAO.seleccionar()
         for usuario in usuarios:
             log.info(usuario)
-
+    elif opcion == 2:
+        username_var = input('Digite el nombre de usuario: ')
+        password_var = input('Digite su contraseña: ')
+        usuario = Usuario(username=username_var, password=password_var)
+        usuario_insertado = UsuarioDAO.insertar(usuario)
+        log.info(f'Usuario insertado: {usuario_insertado}')
 else:
     log.info('Salimos de la aplicacion, Hasta pronto!! ')
 
